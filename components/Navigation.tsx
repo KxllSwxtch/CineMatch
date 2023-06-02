dabb9a7963c82bf8733d497e01b99109d75617be
KxllSwxtch <dimashin9@gmail.com>
2023-06-02 21:05:51 +0900

    updated Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+import Image from 'next/image'
+
 const Navigation: React.FC<{}> = (): JSX.Element => {
+	const [isNavOpen, setNavOpen] = useState<boolean>(false)
+
+	const openNav = () => setNavOpen(true)
+	const closeNav = () => setNavOpen(false)
+
 	return (
 		<>
-			<nav>
-				<ul>
-					<li>Home</li>
-				</ul>
-			</nav>
+			{isNavOpen ? (
+				<nav>
+					<ul>
+						<li onClick={closeNav}>Home</li>
+					</ul>
+				</nav>
+			) : (
+				<Image
+					src='/public/images/menu.png'
+					width={32}
+					height={32}
+					alt='Navigation button'
+					onClick={openNav}
+				/>
+			)}
 		</>
 	)
 }
